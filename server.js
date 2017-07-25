ca534fc1bc9195d789448459c83600696895daca
panditita <topimorita@gmail.com>
2017-07-25 10:51:30 +0100

    Heroku Test

--- server.js
+++ server.js
@@ -19,20 +19,29 @@ app.post("/create-post", function (req, res) {
     const filePath = __dirname + "/data/posts.json";
 
     fs.readFile(filePath, function(error, data){
+        if(error){
+            console.log(error);
+        }
+
         const postsContent = JSON.parse(data);
         const timeStamp = Date.now();
         postsContent[timeStamp] = req.fields.blogpost;
         fs.writeFile(filePath, JSON.stringify(postsContent, null, 2), function() {
-          //  res.send(200, JSON.stringify(postsContent, null, 2));  this returns all posts
-          res.send(200, JSON.stringify(req.fields.blogpost)); //this returns latest post
+            //  res.send(200, JSON.stringify(postsContent, null, 2));  this returns all posts
+           //           console.log (req.fields.blogpost);//this returns latest post
+
+          res.send(200, req.fields.blogpost); 
         });
     });
-
-    //console.log(postsContent);
-
 });
-app.listen(3000, () => {
-    console.log("Server is up on port 3000")
+
+app.get('/get-posts', function (req, res) {
+    res.sendfile(__dirname + "/data/posts.json");
+});
+
+
+app.listen(process.env.PORT || 5000, function () {
+  console.log("Server is listening on port 5000. Ready to accept requests!");
 });
 
 
