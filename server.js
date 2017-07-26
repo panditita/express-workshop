38dc8511fed6730c9825db46ca82be3bae340b10
panditita <topimorita@gmail.com>
2017-07-26 11:52:17 +0100

    Added /about

--- server.js
+++ server.js
@@ -7,41 +7,43 @@ const fs = require("fs");
 app.use(express.static("public"));
 app.use(formidable());
 
-
+//red the file - string
+// change into object - JSON.parse
+// append to the object
+// blogPost[Date.now()] = req.fields.blogpost
+// we write to file JSON.stringify(blogPost)
+//cal back - we return 200 (ok)to client
 app.post("/create-post", function (req, res) {
-    //red the file - string
-    // change into object - JSON.parse
-    // append to the object
-    // blogPost[Date.now()] = req.fields.blogpost
-    // we write to file JSON.stringify(blogPost)
-    //cal back - we return 200 (ok)to client
-
     const filePath = __dirname + "/data/posts.json";
-
-    fs.readFile(filePath, function(error, data){
-        if(error){
+    fs.readFile(filePath, function (error, data) {
+        if (error) {
             console.log(error);
         }
-
         const postsContent = JSON.parse(data);
         const timeStamp = Date.now();
         postsContent[timeStamp] = req.fields.blogpost;
-        fs.writeFile(filePath, JSON.stringify(postsContent, null, 2), function() {
+        fs.writeFile(filePath, JSON.stringify(postsContent, null, 2), function () {
             //  res.send(200, JSON.stringify(postsContent, null, 2));  this returns all posts
-           //           console.log (req.fields.blogpost);//this returns latest post
+            //           console.log (req.fields.blogpost);//this returns latest post
 
-          res.send(200, req.fields.blogpost); 
+            res.send(200, req.fields.blogpost);
         });
     });
 });
 
 app.get('/get-posts', function (req, res) {
-    res.sendfile(__dirname + "/data/posts.json");
+    res.sendFile(__dirname + "/data/posts.json");
 });
 
+app.get('/about', function (req, res) {
+    fs.readFile(__dirname + "/public/about.html", function (error, data) {
+        res.send(data);
+    });
+});
 
 app.listen(process.env.PORT || 5000, function () {
-  console.log("Server is listening on port 5000. Ready to accept requests!");
+    console.log("Server is listening on port 5000. Ready to accept requests!");
 });
+
 
 
